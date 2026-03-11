2b9563cfd8b4da36c79f7509f5263dcf4f80fb64
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-03-11 12:41:26 +0100

    del

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-import plotly.graph_objects as go
 
-def plot_kpi8(pivot_df):
-    df = pivot_df.copy()
-    df.rename(columns={df.columns[0]:"Period"}, inplace=True)
-    fig = go.Figure()
-    for model in df.columns[1:]:
-        fig.add_bar(x=df["Period"].astype(str), y=df[model], name=model)
-    fig.update_layout(title="KPI 8 - Vehicle Volume per Model per Quarter", xaxis_title="Period", yaxis_title="Volume", barmode="group")
-    fig.show()
