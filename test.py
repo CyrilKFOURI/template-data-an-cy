f121d55ce6433e5cb33043b6236ab882c66fed41
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-03-11 11:44:28 +0100

    Z

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-def kpi8_volume_per_model_quarter(self, asset_status):
+def kpi8_volume_per_model_quarter(self, asset_status, end_year):
     df = self.df[self.df["ASSET_STATUS"] == asset_status].copy()
     df = df.dropna(subset=["PLANNED_RETURN_DATE"])
     df["PLANNED_RETURN_DATE"] = pd.to_datetime(df["PLANNED_RETURN_DATE"])
+    
+    df = df[df["PLANNED_RETURN_DATE"].dt.year <= end_year]
     
     df["Quarter"] = df["PLANNED_RETURN_DATE"].dt.to_period("Q")
     
@@ -17,6 +19,11 @@ def kpi8_volume_per_model_quarter(self, asset_status):
     total_row.index = ["Total"]
     
     pivot = pd.concat([pivot, total_row])
+    
+    # Trier les colonnes par le total décroissant
+    col_order = pivot.loc["Total"].sort_values(ascending=False).index
+    pivot = pivot[col_order]
+    
     pivot.reset_index(inplace=True)
     
     return pivot
