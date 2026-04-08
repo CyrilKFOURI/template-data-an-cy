2130f0afc1c4d7a459e93a22f9d60c3619c566b1
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-04-08 13:28:14 +0200

    C

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
-import os
 import pandas as pd
 
 country_map = {
-    "DE": "Germany",
-    "BE": "Belgium",
-    "NL": "Netherlands",
-    "LU": "Luxembourg"
+    "DE": "GERMANY",
+    "BE": "BELGIUM",
+    "NL": "NETHERLANDS",
+    "LU": "LUXEMBOURG"
 }
 
 for country_code in df["COUNTRY"].unique():
@@ -15,6 +14,6 @@ for country_code in df["COUNTRY"].unique():
     df_country = df[df["COUNTRY"] == country_code]
     for yyyymm in df_country["YYYYMM"].unique():
         df_slice = df_country[df_country["YYYYMM"] == yyyymm]
-        output_file = f"/full/path/to/save/{country_name}_{yyyymm}.parquet"
+        output_file = f"/full/path/to/save/NOVA - {country_name} - {yyyymm}.parquet"
         df_slice.to_parquet(output_file, index=False)
         print(f"Saved {output_file}")
