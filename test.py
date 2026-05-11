bf7d346744892d0c9c6d2a75b2940192cda4e6f4
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-05-11 11:21:40 +0200

    Update test.pyr

--- test.py
+++ test.py
@@ -1,19 +1,22 @@
 import numpy as np
 
-# init colonne
-spain_202512["MODEL_2"] = np.nan
+spain_202512_models["MODEL_2"] = np.nan
 
 for brand in a["BRAND"].dropna().unique():
 
-    # subset brand
-    nova_brand = spain_202512[spain_202512["BRAND"] == brand]
+    # dataset a
     a_brand = a[a["BRAND"] == brand]
 
-    for model_a in a_brand["MODEL"].dropna().unique():
+    # dataset nova (spain)
+    nova_brand = spain_202512_models[
+        spain_202512_models["BRAND_UPDATE"] == brand
+    ]
+
+    for model_a in a_brand["MODEL"].dropna().astype(str).unique():
 
         mask = (
-            (spain_202512["BRAND"] == brand) &
-            (spain_202512["MODEL"].str.contains(model_a, na=False))
+            (spain_202512_models["BRAND_UPDATE"] == brand) &
+            (spain_202512_models["MODEL"].astype(str).str.contains(model_a, na=False))
         )
 
-        spain_202512.loc[mask, "MODEL_2"] = model_a
+        spain_202512_models.loc[mask, "MODEL_2"] = model_a
