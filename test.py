fbcfe12e805fb020afe699ecf3d5556e0b54612c
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-05-13 11:06:58 +0200

    Update test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-nova = nova.merge(
-    nova_models[["BRAND_UPDATE", "MODEL", "MODEL_2"]],
-    how="left",
-    on=["BRAND_UPDATE", "MODEL"]
-)
-
-nova["Market_Model"] = nova["MODEL_2"]
-nova.drop(columns=["MODEL_2"], inplace=True)
