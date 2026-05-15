7172ab2df6a1cc890c91ca0f92cdfc11184b624f
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-05-15 17:12:10 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,25 +6,10 @@ def kp19_1_type_share_quarter(df, year, vehicle_type, country, asset_status, bik
     key_cols = ["ID_QUOTATION", "ID_CONTRACT", "VEHICLE_ID"]
     df = df.drop_duplicates(subset=key_cols)
 
-    grouped = df.groupby(["NOVA_ASSET_STATUS", "Quarter"]).size().reset_index(name="COUNT")
+    grouped = df.groupby(["POWER_CATEGORY", "Quarter"]).size().reset_index(name="COUNT")
 
-    pivot = grouped.pivot(index="NOVA_ASSET_STATUS", columns="Quarter", values="COUNT").fillna(0)
+    pivot = grouped.pivot(index="POWER_CATEGORY", columns="Quarter", values="COUNT").fillna(0)
 
     share = pivot.div(pivot.sum(axis=0), axis=1) * 100
 
     return share.round(2)
-
-
-def kp19_2_power_category_per_type_quarter(df, year, vehicle_type, country, asset_status, bike_or_car='CAR'):
-    df = df[(df["YEAR"] == year) & (df["NOVA_ASSET_STATUS"] == asset_status) & (df["MARKET_BODY_GROUP"] == vehicle_type) & (df["COUNTRY"] == country)].copy()
-
-    df["Quarter"] = ((df["MONTH"] - 1) // 3 + 1).apply(lambda x: f"Q{x}")
-
-    key_cols = ["ID_QUOTATION", "ID_CONTRACT", "VEHICLE_ID"]
-    df = df.drop_duplicates(subset=key_cols)
-
-    grouped = df.groupby(["POWER_CATEGORY", "Quarter"]).size().reset_index(name="COUNT")
-
-    pivot = grouped.pivot(index="Quarter", columns="POWER_CATEGORY", values="COUNT").fillna(0)
-
-    return pivot
