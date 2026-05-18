5ce6e27fe8d772bc3093de55039514d5b18ef026
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-05-18 10:47:16 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,18 +10,20 @@ X_train_df = pd.DataFrame(
     columns=[f"f{i}" for i in range(X_train.shape[1])]
 )
 
-X_train_df["target"] = y_train
-X_train_df = X_train_df.dropna()
+df = X_train_df.copy()
+df["target"] = y_train
 
-y_train_series = X_train_df["target"]
-X_train_df = X_train_df.drop(columns=["target"])
+df = df.dropna().reset_index(drop=True)
 
-model.get_booster().feature_names = X_train_df.columns.tolist()
+X_train_clean = df.drop(columns=["target"])
+y_train_clean = df["target"]
+
+model.get_booster().feature_names = X_train_clean.columns.tolist()
 
 viz = dtreeviz(
     model,
-    X_train_df,
-    y_train_series,
+    X_train_clean,
+    y_train_clean,
     target_name="target",
     tree_index=0
 )
