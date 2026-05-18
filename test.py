c86fee6c7114d84aeebce5d1fe12297037f91b7e
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-05-18 10:46:13 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,23 @@
 import pandas as pd
 from dtreeviz.trees import dtreeviz
 
-# 1. récupérer les données depuis la classe
 model = xgboost_simple_model.model
 X_train = xgboost_simple_model.X_train
 y_train = xgboost_simple_model.y_train
 
-# 2. numpy -> DataFrame
 X_train_df = pd.DataFrame(
     X_train,
     columns=[f"f{i}" for i in range(X_train.shape[1])]
 )
 
-# 3. y -> Series
-y_train_series = pd.Series(y_train)
+X_train_df["target"] = y_train
+X_train_df = X_train_df.dropna()
 
-# 4. (optionnel mais recommandé)
+y_train_series = X_train_df["target"]
+X_train_df = X_train_df.drop(columns=["target"])
+
 model.get_booster().feature_names = X_train_df.columns.tolist()
 
-# 5. dtreeviz
 viz = dtreeviz(
     model,
     X_train_df,
