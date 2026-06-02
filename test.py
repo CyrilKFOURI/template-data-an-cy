68df74bc0fb9c77699b0c57aae23c0e957bd7dc5
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-06-02 11:24:36 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import pyarrow.parquet as pq
 pf = pq.ParquetFile(r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data\Parquets\NOVA - TR - 202402.parquet")
 pf2 = pq.ParquetFile(r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data\Parquets\NOVA - TR - 202401.parquet")
 
-table2 = pf2.read()
-table2 = table2.replace_schema_metadata(pf.schema_arrow.metadata)
-table2 = table2.cast(pf.schema_arrow, safe=False)
+table = pf2.read()
+table = table.cast(pf.schema_arrow, safe=False)
 
-pq.write_table(table2, r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data\Parquets\NOVA - TR - 202402_fixed.parquet")
+pq.write_table(table, r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data\Parquets\NOVA - TR - 202402_fixed.parquet")
