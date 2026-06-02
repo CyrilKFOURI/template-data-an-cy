36efb13c61e79285bec5c734f8ecd08bc5be3bc3
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-06-02 11:05:40 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ pf2 = pq.ParquetFile(r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data
 
 df2 = pf2.read().to_pandas()
 df2.columns = pf.schema.names
-
-df2.head()
+df2 = df2.loc[:, ~df2.columns.duplicated()]
+df2.to_parquet(r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data\Parquets\NOVA - TR - 202402_fixed.parquet", index=False)
