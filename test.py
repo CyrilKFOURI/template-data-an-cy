823c95e7701545039938f7db0af629e3aa5406b3
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-06-02 11:01:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import pyarrow.parquet as pq
 
 pf = pq.ParquetFile(r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data\Parquets\NOVA - TR - 202402.parquet")
-pf_2 = pq.ParquetFile(r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data\Parquets\NOVA - TR - 202401.parquet")
+pf2 = pq.ParquetFile(r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data\Parquets\NOVA - TR - 202401.parquet")
 
-df = pf.read().to_pandas()
-df.columns = pf_2.schema.names
+df2 = pf2.read().to_pandas()
+df2.columns = pf.schema.names
 
-df.head()
+df2.head()
