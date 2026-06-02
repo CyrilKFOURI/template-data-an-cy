9c4737ffac06b09795db8628446e5500deb33c51
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-06-02 11:22:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
+import pyarrow as pa
 import pyarrow.parquet as pq
 
 pf = pq.ParquetFile(r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data\Parquets\NOVA - TR - 202402.parquet")
 pf2 = pq.ParquetFile(r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data\Parquets\NOVA - TR - 202401.parquet")
 
-df2 = pf2.read().to_pandas()
-df2.columns = pf.schema.names
-df2 = df2.loc[:, ~df2.columns.duplicated()]
-df2.to_parquet(r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data\Parquets\NOVA - TR - 202402_fixed.parquet", index=False)
+table2 = pf2.read()
+schema1 = pf.schema_arrow
+
+table2 = table2.cast(schema1, safe=False)
+
+pq.write_table(table2, r"C:\Users\j21958\OneDrive - BNP Paribas\Documents\Nov\Data\Parquets\NOVA - TR - 202402_fixed.parquet")
