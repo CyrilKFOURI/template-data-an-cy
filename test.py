a3958a596c410bb199c0a16004875def7299905a
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-06-04 14:54:14 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,21 @@
 import os
 import glob
 
-folder = "chemin/vers/tes/parquets"
+folder = "ton_path"
 
-for f in glob.glob(f"{folder}/*.parquet"):
+files = glob.glob(f"{folder}/*.parquet")
+
+print(f"{len(files)} fichiers trouvés")
+
+for f in files:
     base = os.path.basename(f)
+    print(base)
 
     if "-merged" in base:
         new_base = base.replace("-merged", "")
-        old_path = f
-        new_path = os.path.join(folder, new_base)
+        print(f"RENAME: {base} -> {new_base}")
 
-        os.rename(old_path, new_path)
-
-        print(f"{base} -> {new_base}")
+        os.rename(
+            f,
+            os.path.join(folder, new_base)
+        )
