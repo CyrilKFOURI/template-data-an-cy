1f563b322e48bfbd6b7f322759fef3eaa6790779
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-06-05 10:06:13 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 Bonjour Mouayed,
 
-Je t’écris car nous n’avons pas la règle permettant de savoir comment les pays classifient les types de véhicules, notamment pour déterminer si un véhicule doit être considéré comme PV (Passenger Vehicle) ou LCV (Light Commercial Vehicle).
+Je te contacte pour te demander quelque chose concernant les types de véhicules.
+
+Nous n’avons actuellement pas la règle permettant de savoir comment les pays classifient les véhicules, notamment pour déterminer si un modèle doit être considéré comme PV (Passenger Vehicle) ou LCV (Light Commercial Vehicle).
 
 Je voulais savoir si tu pouvais te rapprocher des équipes concernées afin de comprendre quels critères ou quelles règles sont utilisés dans les différents pays pour effectuer cette classification.
 
-Merci d’avance pour ton aide.
+Merci d’avance pour ton aide et ton retour.
 
 Bien à toi,
 
