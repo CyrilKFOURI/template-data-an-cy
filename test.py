6cff608639d293805896a7686e233a8e97d8b518
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-06-18 13:38:13 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 import pandas as pd
+import json
+
+# Récupérez la chaîne de caractères
+json_str = wltp["data_json"].iloc[0]
+
+# Convertissez la chaîne JSON en un objet Python (liste ou dictionnaire)
+data = json.loads(json_str)
+
+# Maintenant, créez le DataFrame
+tmp = pd.DataFrame(data)
+
+# Vous pouvez ensuite utiliser votre code Plotly
 import plotly.express as px
-
-tmp = pd.DataFrame(wltp["data_json"].iloc[0])
-
-fig = px.line(
-    tmp,
-    x="QTR",
-    y="WLTP_Q_MEAN_VEH_NEW",
-    markers=True
-)
-
+fig = px.line(tmp, x="QTR", y="WLTP_Q_MEAN_VEH_NEW", markers=True)
 fig.show()
