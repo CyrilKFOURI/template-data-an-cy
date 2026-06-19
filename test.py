d88931b1f3a484bf7e82b52de163246ef758e50e
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-06-19 10:47:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
-import pandas as pd
-import json
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Récupérez la chaîne de caractères
-json_str = wltp["data_json"].iloc[0]
+def get_grouped_data(df, y_col):
+    # Prépare le dataframe agrégé pour la heatmap
+    df_grouped = df.groupby([y_col, 'ARVAL_INDUSTRY_CODE_CLS_DESCRIPTION', 'CLS_GROUP_RATING']).size().reset_index(name='count')
+    return df_grouped.pivot_table(index=y_col, columns='ARVAL_INDUSTRY_CODE_CLS_DESCRIPTION', values='count', aggfunc='sum', fill_value=0)
 
-# Convertissez la chaîne JSON en un objet Python (liste ou dictionnaire)
-data = json.loads(json_str)
-
-# Maintenant, créez le DataFrame
-tmp = pd.DataFrame(data)
-
-# Vous pouvez ensuite utiliser votre code Plotly
-import plotly.express as px
-fig = px.line(tmp, x="QTR", y="WLTP_Q_MEAN_VEH_NEW", markers=True)
-fig.show()
+def plot_arval_heatmap(df, y_col):
+    # Génère et affiche la heatmap
+    data = get_grouped_data(df, y_col)
+    plt.figure(figsize=(14, 10))
+    sns.heatmap(data, annot=True, fmt="d", cmap="YlGnBu")
+    plt.title(f'Distribution: {y_col} par Segment')
+    plt.show()
