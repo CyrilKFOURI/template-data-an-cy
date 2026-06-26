93bd781a0b9afda896166387960a0db4bd65690e
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-06-26 18:29:27 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-a = pd.crosstab(
-    index=nova['COUNTRY'], 
-    columns=nova['CDN_CLF_SEGMENT'], 
-    values=nova['BIKE_OR_CAR'],  # La colonne que vous voulez agréger
-    aggfunc='count'              # 'count', 'sum', 'mean', 'max', etc.
+# Si vous voulez voir les valeurs de BIKE_OR_CAR dans le tableau :
+a = pd.pivot_table(
+    nova, 
+    index='COUNTRY', 
+    columns='CDN_CLF_SEGMENT', 
+    values='BIKE_OR_CAR', 
+    aggfunc='first' # Affiche la première valeur trouvée pour ce croisement
 )
