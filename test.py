a45c697c203493621faa74f58df1949f8e3fd913
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-06-26 18:27:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
-# Mettez à jour votre dictionnaire avec les valeurs manquantes si nécessaire
-# ou gardez-le tel quel pour laisser les autres valeurs tranquilles
-mapping_segments = {
-    "A": "City",
-    "B": "Mid Small",
-    "C": "Mid Large",
-    "D": "Mid Large",
-    "E": "High / Luxury",
-    "F": "High / Luxury"
-}
-
-# Utilisez replace() au lieu de map()
-nova["CDN_CLF_SEGMENT"] = nova["CDN_CLF_SEGMENT"].replace(mapping_segments)
+a = pd.crosstab(
+    index=nova['COUNTRY'], 
+    columns=nova['CDN_CLF_SEGMENT'], 
+    values=nova['BIKE_OR_CAR'],  # La colonne que vous voulez agréger
+    aggfunc='count'              # 'count', 'sum', 'mean', 'max', etc.
+)
