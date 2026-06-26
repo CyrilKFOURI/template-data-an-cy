9e246fc9c1e86e31c1491c898974186847094ba7
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-06-26 17:39:01 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-# Pour avoir Pays en ligne, puis Variable_A, et en colonnes Variable_B
-df_crosstab = pd.crosstab(
-    index=[df['Pays'], df['Variable_A']], 
-    columns=df['Variable_B']
+# Définition du dictionnaire de correspondance
+mapping_segments = {
+    "A": "City",
+    "B": "Mid Small",
+    "C": "Mid Large",
+    "D": "Mid Large",
+    "E": "High / Luxury",
+    "F": "High / Luxury"
+}
+
+# Remplacement direct dans la colonne source
+nova["CON_CLF_SEGMENT"] = nova["CON_CLF_SEGMENT"].map(mapping_segments)
+
+# Ensuite, vous pouvez exécuter votre crosstab normalement
+df_1 = pd.crosstab(
+    index=[nova["NORMALISED_VEHICLE_TYPE"], nova["CON_CLF_SEGMENT"]], 
+    columns=nova["COUNTRY"]
 )
+
