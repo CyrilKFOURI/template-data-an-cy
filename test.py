45551e95349878338e958d025b06c7daaafbdebb
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-07-08 10:10:45 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 
+top10 = (
+    df["VEHICLE_MODEL"]
+    .value_counts()
+    .head(10)
+    .rename_axis("VEHICLE_MODEL")
+    .reset_index(name="Count")
+)
+
+# Ajouter la marque correspondante
+top10["BRAND"] = top10["VEHICLE_MODEL"].map(
+    df.drop_duplicates("VEHICLE_MODEL").set_index("VEHICLE_MODEL")["BRAND"]
+)
+
+print(top10)
