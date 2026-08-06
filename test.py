140841330b0e288c25ba5c8739cccef811137578
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-08-06 11:00:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,33 +1,56 @@
 counts = (
     nova
-    .groupby(["COUNTRY", "MARKET_MODEL"])
+    .groupby(["COUNTRY", "MODEL"])
     .size()
-    .reset_index(name="Volume")
+    .reset_index(name="Count")
 )
 
 top10_par_pays = (
     counts
-    .sort_values(["COUNTRY", "Volume"], ascending=[True, False])
+    .sort_values(["COUNTRY", "Count"], ascending=[True, False])
     .groupby("COUNTRY")
     .head(10)
+    .drop(columns="Count")
     .reset_index(drop=True)
-)
-
-top10_par_pays["Share (%)"] = (
-    top10_par_pays
-    .groupby("COUNTRY")["Volume"]
-    .transform(lambda x: (x / x.sum() * 100).round(1))
 )
 
 brand_map = (
     nova
-    .drop_duplicates(["COUNTRY", "MARKET_MODEL"])
-    .set_index(["COUNTRY", "MARKET_MODEL"])["BRAND_UPDATE"]
+    .drop_duplicates(["COUNTRY", "MODEL"])
+    .set_index(["COUNTRY", "MODEL"])["BRAND"]
 )
 
-top10_par_pays["BRAND_UPDATE"] = (
+top10_par_pays["BRAND"] = (
     top10_par_pays
-    .set_index(["COUNTRY", "MARKET_MODEL"])
+    .set_index(["COUNTRY", "MODEL"])
     .index
     .map(brand_map)
 )
+
+
+
+counts = (
+    nova
+    .groupby("MODEL")
+    .size()
+    .reset_index(name="Count")
+)
+
+top10_global = (
+    counts
+    .sort_values("Count", ascending=False)
+    .head(10)
+    .drop(columns="Count")
+    .reset_index(drop=True)
+)
+
+brand_map = (
+    nova
+    .drop_duplicates("MODEL")
+    .set_index("MODEL")["BRAND"]
+)
+
+top10_global["BRAND"] = (
+    top10_global["MODEL"].map(brand_map)
+)
+
