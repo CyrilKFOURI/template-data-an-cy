83d644acf99ea20a05cc72c4994b4e816c10bd88
Cyril Kfouri <95475725+CyrilKFOURI@users.noreply.github.com>
2026-08-17 10:19:05 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
+import pandas as pd
+
+error_cols = []
 
 for col in df.columns:
     try:
         df[[col]].to_parquet("/tmp/test.parquet", index=False)
     except Exception as e:
-        print(f"❌ {col} : {e}")
+        if "Expected bytes, got" in str(e):
+            error_cols.append(col)
+
+print(error_cols)
